c83e524697cf6026d62e8022e997fc6255c76f11
Mathias Böhm <mathias.boehm@struktuhr.de>
2026-02-09 08:44:23 +0100

    switch to spring boot 4

--- etc/sql/postgres_create-tables.sql
+++ etc/sql/postgres_create-tables.sql
@@ -1,1 +1,12 @@
-select * from task;
+drop table task;
+
+CREATE TABLE task(
+                     id SERIAL PRIMARY KEY,
+                     version INTEGER,
+                     name VARCHAR(50) NOT NULL,
+                     owner VARCHAR(200),
+                     status VARCHAR(10) NOT NULL,
+                     created TIMESTAMP,
+                     claimed TIMESTAMP,
+                     finished TIMESTAMP
+);
--- src/test/java/de/struktuhr/demo/TestHelper.java
+++ src/test/java/de/struktuhr/demo/TestHelper.java
@@ -1,17 +1,18 @@
 package de.struktuhr.demo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 public class TestHelper {
 
     public static void printJson(Object obj) {
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new JsonMapper();
         try {
             String s = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
             System.out.println(s);
         }
-        catch (JsonProcessingException e) {
+        catch (Exception e) {
            throw new RuntimeException(e);
         }
     }
